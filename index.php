a1b0511366ca5b9ea32ae2469069fa90f00e8519
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-06-12 16:10:22 +0200

    Milestone 3 - Fatta

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+$userPassword = $_GET["userPassword"];
+// A quanto pare questo non funziona in questo caso, da chiedere
+// include 'index.php';
+$numero_caratteri = (int)$_GET['userPassword'];
+$allowedChars = "qwertyuiopasdfghjklzxcvbnm";
+
+function getName($n)
+{
+    $allowedChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($allowedChars) - 1);
+        $randomString .= $allowedChars[$index];
+    }
+
+    return $randomString;
+}
+$randomString = getName($numero_caratteri);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,10 +30,24 @@
 </head>
 
 <body>
-    <form action="form.php">
-        <input type="number"placeholder="Inserisci il numero di cattareri che desideri all'interno della tua password" name="userPassword">
+    <form>
+        <input type="number" placeholder="Inserisci il numero di cattareri che desideri all'interno della tua password" name="userPassword" min="1">
         <button type="submit">Invia</button>
+        <button type="submit">Reset</button>
+        <!-- Come creo faccio in modo che al click esegua una funzione specifica? -->
     </form>
+
+
+    <h1><?php if ($numero_caratteri > 0) {
+
+            echo ($numero_caratteri);
+            getName($numero_caratteri);
+            echo "<span>" . "$randomString";
+        }
+        ?>
+
+    </h1>
+
 </body>
 
 </html>
